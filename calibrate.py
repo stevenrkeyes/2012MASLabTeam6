aece9a6fd1191a636d9411995b8c70ec6de15ffb
William Livernois <wliverno@mit.edu>
2013-01-17 12:00:17 -0500

    Added circle to calibration file.

--- calibrate.py
+++ calibrate.py
@@ -1,11 +1,12 @@
 import cv, time
 
 if __name__ == "__main__":
-	cam = cv.CaptureFromCAM(0)
+	cam = cv.CaptureFromCAM(-1)
 	while True:
 		feed = cv.QueryFrame(cam)
 		feed_hsv = cv.CreateImage((feed.width, feed.height), cv.IPL_DEPTH_8U, 3)
 		cv.CvtColor(feed, feed_hsv, cv.CV_RGB2HSV);
+		cv.Circle(feed, (feed.width/2, feed.height/2), 40, cv.Scalar(100,200,200),  3)
 		point = cv.Get2D(feed_hsv,  feed.height/2, feed.width/2)
 		print "H: ", point[0], "\tS:", point[1], "\tV:", point[2]
 		time.sleep(0.5)
