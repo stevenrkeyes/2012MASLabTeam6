6fa7eea664965132bb84b40d9254a1f857f8cf02
Steven Keyes <stevenrkeyes@gmail.com>
2013-01-21 18:00:01 -0500

    roller code
    made roller.py to control the intake roller

---
+++ roller.py
@@ -0,0 +1,22 @@
+import arduino, time
+
+# class that is a controller for the roller motor
+class Roller:
+    def __init__(self, ard):
+        self.ard = ard
+        self.mRoller = arduino.Motor(ard, 11, 12, 13)
+
+    def startRoller(self):
+        self.mRoller.setSpeed(120)
+
+    def stopRoller(self):
+        self.mRoller.setSpeed(0)
+
+if __name__ == "__main__":
+    ard = arduino.Arduino()
+    r = Roller(ard)
+
+    ard.run()
+    r.startRoller()
+    time.sleep(5)
+    r.stopRoller()
